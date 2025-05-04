3f65f3c4cd468b27943031c5525ceff67f38dd65
Daniel Quán <113466726+DQUANC@users.noreply.github.com>
2025-05-04 09:26:14 -0600

    Add files via upload

---
+++ Exercise1_Apr05.ts
@@ -0,0 +1,12 @@
+const number0 = 2;
+const number1 = 3;
+const number2 = 10;
+
+function sumNumbers(n0, n1, n2){
+    let SumNumbers = n0 + n1 + n2;
+    return SumNumbers;
+};
+
+for (let i = 0; i <= sumNumbers(number0, number1, number2); i++){
+    console.log(i + "*");
+};
